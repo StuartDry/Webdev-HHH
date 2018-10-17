b36fc980b9eaee1668a9f59d187b65402049f1e1
Unknownn42 <stuart.drysdale@student.aventus.nl>
2018-10-17 16:06:52 +0200

    update

--- includes/homepage.php
+++ includes/homepage.php
@@ -1,5 +1,241 @@
-<p>Dit is de homepage</p>
+
+<!-- link voor icons -->
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
+
+
+<div class="mt-5"></div>
+<section class="breadcrumb">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="breadcrumb m-0 p-0 pt-2">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item"><a href="#">Fruit</a></li>
+                    <li class="breadcrumb-item active">Pears</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="listings">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2> Reno: 42 properties found </h2>
+                        <p> 3 Reasons to Visit: gambling, entertainment & relaxation </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="nav nav-justified nav-pills">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Active</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link bg-light" href="#">Active</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link bg-light" href="#">Active</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link bg-light" href="#">Active</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link bg-light" href="#">Active</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="https://dynamic.realestateindia.com/proj_images/project14122/proj_img-14122_1-small.jpg">
+                                    </div>
+                                    <div class="col-md-6  card-body">
+                                        <div class="list-title">
+                                            <ul class="list-inline list-unstyled">
+                                                <li class="list-inline-item"><a href="#"><h4>Nugget Casino Resort</h4></a></li>
+                                                <li class="list-inline-item text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i></li>
+                                                <li class="list-inline-item text-success"><i class="fa fa-thumbs-up"></i></li>
+                                            </ul>
+                                        </div>
+                                        <div class="list-location">
+                                            <a href="#"><i class="fa fa-map-marker"></i><small> Sparks, Reno – Show on map (4.7 km from center)</small> </a>
+                                        </div>
+                                        <div class="list-descrip">
+                                            <small>This Sparks casino hotel is located in the Sierra Nevada Mountains. This resort features free airport shuttle services, a casino, a nightclub and 8 restaurants and bars. </small>
+                                        </div>
 
 
 
+                                    </div>
+                                    <div class="col-md-3 border-left card-body">
+                                        <ul class="list-unstyled">
+                                            <li><h3>Very Good</h3></li>
+                                            <li class="text-secondary"><small>8067 Reviews  </small></li>
+                                        </ul>
+                                        <button type="button" class="btn btn-outline-primary">Book Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="https://dynamic.realestateindia.com/proj_images/project14122/proj_img-14122_1-small.jpg">
+                                    </div>
+                                    <div class="col-md-6  card-body">
+                                        <div class="list-title">
+                                            <ul class="list-inline list-unstyled">
+                                                <li class="list-inline-item"><a href="#"><h4>Nugget Casino Resort</h4></a></li>
+                                                <li class="list-inline-item text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i></li>
+                                                <li class="list-inline-item text-success"><i class="fa fa-thumbs-up"></i></li>
+                                            </ul>
+                                        </div>
+                                        <div class="list-location">
+                                            <a href="#"><i class="fa fa-map-marker"></i><small> Sparks, Reno – Show on map (4.7 km from center)</small> </a>
+                                        </div>
+                                        <div class="list-descrip">
+                                            <small>This Sparks casino hotel is located in the Sierra Nevada Mountains. This resort features free airport shuttle services, a casino, a nightclub and 8 restaurants and bars. </small>
+                                        </div>
 
+
+
+                                    </div>
+                                    <div class="col-md-3 border-left card-body">
+                                        <ul class="list-unstyled">
+                                            <li><h3>Very Good</h3></li>
+                                            <li class="text-secondary"><small>8067 Reviews  </small></li>
+                                        </ul>
+                                        <button type="button" class="btn btn-outline-primary">Book Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="https://dynamic.realestateindia.com/proj_images/project14122/proj_img-14122_1-small.jpg">
+                                    </div>
+                                    <div class="col-md-6  card-body">
+                                        <div class="list-title">
+                                            <ul class="list-inline list-unstyled">
+                                                <li class="list-inline-item"><a href="#"><h4>Nugget Casino Resort</h4></a></li>
+                                                <li class="list-inline-item text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i></li>
+                                                <li class="list-inline-item text-success"><i class="fa fa-thumbs-up"></i></li>
+                                            </ul>
+                                        </div>
+                                        <div class="list-location">
+                                            <a href="#"><i class="fa fa-map-marker"></i><small> Sparks, Reno – Show on map (4.7 km from center)</small> </a>
+                                        </div>
+                                        <div class="list-descrip">
+                                            <small>This Sparks casino hotel is located in the Sierra Nevada Mountains. This resort features free airport shuttle services, a casino, a nightclub and 8 restaurants and bars. </small>
+                                        </div>
+
+
+
+                                    </div>
+                                    <div class="col-md-3 border-left card-body">
+                                        <ul class="list-unstyled">
+                                            <li><h3>Very Good</h3></li>
+                                            <li class="text-secondary"><small>8067 Reviews  </small></li>
+                                        </ul>
+                                        <button type="button" class="btn btn-outline-primary">Book Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="https://dynamic.realestateindia.com/proj_images/project14122/proj_img-14122_1-small.jpg">
+                                    </div>
+                                    <div class="col-md-6  card-body">
+                                        <div class="list-title">
+                                            <ul class="list-inline list-unstyled">
+                                                <li class="list-inline-item"><a href="#"><h4>Nugget Casino Resort</h4></a></li>
+                                                <li class="list-inline-item text-warning"><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half"></i></li>
+                                                <li class="list-inline-item text-success"><i class="fa fa-thumbs-up"></i></li>
+                                            </ul>
+                                        </div>
+                                        <div class="list-location">
+                                            <a href="#"><i class="fa fa-map-marker"></i><small> Sparks, Reno – Show on map (4.7 km from center)</small> </a>
+                                        </div>
+                                        <div class="list-descrip">
+                                            <small>This Sparks casino hotel is located in the Sierra Nevada Mountains. This resort features free airport shuttle services, a casino, a nightclub and 8 restaurants and bars. </small>
+                                        </div>
+
+
+
+                                    </div>
+                                    <div class="col-md-3 border-left card-body">
+                                        <ul class="list-unstyled">
+                                            <li><h3>Very Good</h3></li>
+                                            <li class="text-secondary"><small>8067 Reviews  </small></li>
+                                        </ul>
+                                        <button type="button" class="btn btn-outline-primary">Book Now</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <small> 42 properties found in Reno.  </small>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="pagination">
+                            <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="cta py-5 bg-primary text-white">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h2>Save time, save money!</h2>
+                <p> Sign up and we'll send the best deals to you </p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="cta-btm py-2 bg-info">
+    <div class="row text-center">
+        <div class="col-md-12 ">
+            <button type="button" class="btn btn-outline-light">Book Now</button>
+            <button type="button" class="btn btn-outline-light">Sell Services</button>
+        </div>
+    </div>
+</section>
