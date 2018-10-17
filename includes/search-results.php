637b21bdc4c5ec54b8a37e5f88bf28040a40133d
Unknownn42 <stuart.drysdale@student.aventus.nl>
2018-10-17 17:23:39 +0200

    login fancy

--- includes/search-results.php
+++ includes/search-results.php
@@ -1,7 +1,6 @@
 <!-- link voor icons -->
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-T8Gy5hrqNKT+hzMclPo118YTQO6cYprQmhrYwIiQ/3axmI1hQomh7Ud2hPOy8SP1" crossorigin="anonymous">
 
-
 <div class="mt-5"></div>
 <section class="breadcrumb">
     <div class="container">
@@ -19,6 +18,198 @@
 <section class="listings">
     <div class="container">
         <div class="row">
+            <div class="col-md-3">
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card bg-warning">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h3>Search</h3>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form>
+                                            <div class="form-group">
+                                                <select class="form-control" id="conditionsselect1">
+                                                    <option>Conditions</option>
+                                                    <option>New</option>
+                                                    <option>Used</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="conditionsselect2">
+                                                    <option>Body</option>
+                                                    <option>Compact</option>
+                                                    <option>Convertible</option>
+                                                    <option>Coupe</option>
+                                                    <option>Off-road</option>
+                                                    <option>Sedan</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="make1">
+                                                    <option>Make</option>
+                                                    <option>Ford</option>
+                                                    <option>Hyundai</option>
+                                                    <option>Kia</option>
+                                                    <option>Honda</option>
+                                                    <option>Skoda</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="make1">
+                                                    <option>Model</option>
+                                                    <option>Accord</option>
+                                                    <option>Camry</option>
+                                                    <option>Civic</option>
+                                                    <option>Rapid</option>
+                                                    <option>Octavia</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="mileage1">
+                                                    <option>Max Mileage</option>
+                                                    <option>< 5000</option>
+                                                    <option>5000-10000</option>
+                                                    <option>10000-15000</option>
+                                                    <option>15000-20000</option>
+                                                    <option>20000-25000</option>
+                                                    <option>> 25000</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="year1">
+                                                    <option>Year</option>
+                                                    <option>2000</option>
+                                                    <option>2001</option>
+                                                    <option>2002</option>
+                                                    <option>2003</option>
+                                                    <option>2004</option>
+                                                    <option>2005</option>
+                                                    <option>2006</option>
+                                                    <option>2007</option>
+                                                    <option>2008</option>
+                                                    <option>2009</option>
+                                                    <option>2010</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="make1">
+                                                    <option>Transmission</option>
+                                                    <option>Automatic</option>
+                                                    <option>Manaul</option>
+                                                    <option>Semi-Automatic</option>
+                                                </select>
+                                            </div>
+                                            <hr>
+                                            <button type="btn" class="btn btn-primary">Find Now</button>
+                                            <button type="btn" class="btn btn-primary">Reset All</button>
+                                            <div class="pb-3"></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row ">
+                    <div class="col-md-12">
+                        <div class="card bg-primary text-white">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <h3>Search</h3>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <form>
+                                            <div class="form-group">
+                                                <select class="form-control" id="conditionsselect1">
+                                                    <option>Conditions</option>
+                                                    <option>New</option>
+                                                    <option>Used</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="conditionsselect2">
+                                                    <option>Body</option>
+                                                    <option>Compact</option>
+                                                    <option>Convertible</option>
+                                                    <option>Coupe</option>
+                                                    <option>Off-road</option>
+                                                    <option>Sedan</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="make1">
+                                                    <option>Make</option>
+                                                    <option>Ford</option>
+                                                    <option>Hyundai</option>
+                                                    <option>Kia</option>
+                                                    <option>Honda</option>
+                                                    <option>Skoda</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="make1">
+                                                    <option>Model</option>
+                                                    <option>Accord</option>
+                                                    <option>Camry</option>
+                                                    <option>Civic</option>
+                                                    <option>Rapid</option>
+                                                    <option>Octavia</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="mileage1">
+                                                    <option>Max Mileage</option>
+                                                    <option>< 5000</option>
+                                                    <option>5000-10000</option>
+                                                    <option>10000-15000</option>
+                                                    <option>15000-20000</option>
+                                                    <option>20000-25000</option>
+                                                    <option>> 25000</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="year1">
+                                                    <option>Year</option>
+                                                    <option>2000</option>
+                                                    <option>2001</option>
+                                                    <option>2002</option>
+                                                    <option>2003</option>
+                                                    <option>2004</option>
+                                                    <option>2005</option>
+                                                    <option>2006</option>
+                                                    <option>2007</option>
+                                                    <option>2008</option>
+                                                    <option>2009</option>
+                                                    <option>2010</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <select class="form-control" id="make1">
+                                                    <option>Transmission</option>
+                                                    <option>Automatic</option>
+                                                    <option>Manaul</option>
+                                                    <option>Semi-Automatic</option>
+                                                </select>
+                                            </div>
+                                            <hr>
+                                            <button type="btn" class="btn btn-primary">Find Now</button>
+                                            <button type="btn" class="btn btn-primary">Reset All</button>
+                                            <div class="pb-3"></div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="col-md-9">
                 <div class="row">
                     <div class="col-md-12">
@@ -231,10 +422,10 @@
     </div>
 </section>
 <section class="cta-btm py-2 bg-info">
-
-
+    <div class="row text-center">
+        <div class="col-md-12 ">
             <button type="button" class="btn btn-outline-light">Book Now</button>
             <button type="button" class="btn btn-outline-light">Sell Services</button>
-
-
+        </div>
+    </div>
 </section>
