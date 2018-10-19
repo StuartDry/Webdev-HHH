528e374d6e1f1cc4cee3f46bd02fcb6a6f2f3e08
Iris <iris.jansen@student.aventus.nl>
2018-10-19 14:55:47 +0200

    boeking wordt in db opgeslagen

---
+++ Classes/Order.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Iris
+ * Date: 19-10-2018
+ * Time: 14:13
+ */
+
+class Order
+{
+
+
+    public function __construct(){
+
+    }
+
+    public function saveOrder($userID, $parkID, $homeID, $checkin_date, $checkout_date){
+
+        $db = Database::getInstance();
+        $conn = $db->getConnection();
+
+        $sql='INSERT INTO `order` (userID, parkID, homeID, `check-in_date`, `check-out_date`) VALUES 
+        ("'.$conn->real_escape_string($userID).'", 
+        "'.$conn->real_escape_string($parkID).'", 
+        "'.$conn->real_escape_string($homeID).'", 
+        "'.$conn->real_escape_string($checkin_date).'", 
+        "'.$conn->real_escape_string($checkout_date).'"
+        )';
+        if($conn->query($sql)){
+            echo "Boeken gelukt.";
+            return $conn->insert_id;
+        }else{var_dump($sql);}
+        echo("Error description: " . mysqli_error($conn));
+        return false;
+    }
+}
--- includes/checkout.php
+++ includes/checkout.php
@@ -4,13 +4,32 @@
  * User: Iris
  * Date: 18-10-2018
  * Time: 21:18
- */?>
+ */
+include 'Classes/Order.php';
+if(isset($_POST['checkout_btn'])){
+    $userID=$_SESSION['user']['userID'];
+    $parkID = 1;
+    $homeID=$_SESSION['cart'][0][0];
+    $checkin_date=$_SESSION['cart'][0][1];
+    $checkout_date=$_SESSION['cart'][0][2];
+
+//    echo "<pre>";
+//    print_r($_SESSION);
+//    echo "</pre>";
+
+    $order = new Order();
+    $result=$order->saveOrder($userID, $parkID, $homeID, $checkin_date, $checkout_date);
+    if(!$result){
+        echo "Boeken mislukt.";
+    }
+}
+?>
 
 <div class="container checkout_container">
     <form action="" method="post">
         <div class="row">
             <div class="col-xs-2 col-sm-1">
-                <?php //TODO labels aan inputs meegeven! ?>
+                <?php //TODO labels aan inputs meegeven! radio buttons kunnen nu tegelijk worden ingedrukt... ?>
                 <input type="radio" name="ideal_payment">
             </div>
             <div class="col-xs-10 col-sm-11">
@@ -31,7 +50,9 @@
             </div>
         </div>
         <div class="row">
-            <input type="submit" class="btn btn-success checkout_btn" name="checkout_btn" value="Proceed to payment">
+            <form action="" method="post">
+                <input type="submit" class="btn btn-success checkout_btn" name="checkout_btn" value="Proceed to payment">
+            </form>
         </div>
     </form>
 </div>
